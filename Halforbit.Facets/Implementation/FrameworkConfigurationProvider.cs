2d739b2e10bfee76c2f4097d14c5df59a539f365
Jim Noble <none@none>
2018-12-20 14:28:30 -0500

    add framework config provider

---
+++ Halforbit.Facets/Implementation/FrameworkConfigurationProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Halforbit.Facets.Implementation
+{
+    class FrameworkConfigurationProvider : Halforbit.Facets.Interface.IConfigurationProvider
+    {
+        IConfiguration _configuration;
+
+        public FrameworkConfigurationProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetValue(string key)
+        {
+            return _configuration[key];
+        }
+    }
+}
